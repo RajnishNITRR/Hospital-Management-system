2aa1ada7e208153fcb650212b7d6f59014829717
Rajnish Kumar <rajnish1800084@gmail.com>
2024-11-11 02:08:18 +0530

    backend url fix

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -15,7 +15,7 @@ const MessageForm = () => {
     try {
       await axios
         .post(
-          `${process.env.VITE_BACKEND_URL}/api/v1/message/send`,
+          `${import.meta.env.VITE_BACKEND_URL}/api/v1/message/send`,
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
